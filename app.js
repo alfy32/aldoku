37b79de8091481c703a637d03ea7b9b3f7c160d5
Alan Christensen <alfy32@gmail.com>
2013-12-05 16:52:54 -0700

    Made a simple express app to serve the pages.

---
+++ app.js
@@ -0,0 +1,29 @@
+/* jshint node:true */
+'use strict';
+
+var express = require('express');
+var http = require('http');
+var config = require('config');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || config.port);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static('client'));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
